6bde754c36822ab45db053af45dad76254d85400
unknown <goyalaayush93@gmail.com>
2022-12-23 23:06:03 +0530

    minor changes

--- user/models.py
+++ user/models.py
@@ -33,8 +33,7 @@ class PositionDetails(models.Model):
 
 class Newsletter(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    event_sub = models.BooleanField(default=False)
-    fest_sub = models.BooleanField(default=False)
+    sub = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
