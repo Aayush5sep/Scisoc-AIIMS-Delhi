b41e5d191cff7a7e5fa311d82742b007b6e4d0b4
unknown <goyalaayush93@gmail.com>
2022-12-31 20:54:00 +0530

    Quiz Model & Admin Display for Medquiz

--- medquiz/admin.py
+++ medquiz/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
-
+from .models import Quiz,Registration,Question,Choice,Solution
 # Register your models here.
+
+class RegistrationAdmin(admin.StackedInline):
+    model = Registration
+    extra = 0
+
+class QuizAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Quiz Display', {'fields':['title','desc','reg_price','team_cnt']}),
+        ('Optional Display', {'fields':['reg_open','quiz_live']}),
+        ('Conducting Platform', {'fields':['on_web','quiz_or_reg_link']}),
+    ]
+    readonly_fields = ('id',)
+    inlines = [RegistrationAdmin]
+
+class ChoiceAdmin(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Details', {'fields':['quiz_model','question_detail','image','qn_image','answer','marks']}),
+        ('Question Type', {'fields':['choices','short_answer','numerical','true_false','matchup','num_match']}),
+    ]
+    readonly_fields = ('qid','qn_image')
+    inlines = [ChoiceAdmin]
+
+class SolutionAdmin(admin.StackedInline):
+    model = Solution
+    readonly_fields = ['quiz_id','question_detail','sol_by_participant','qn_image']
+    extra = 0
+
+class RegisteredAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Registration Details', {'fields':['reg_id','registered','payment_id','quiz_model']}),
+        ('Team Details', {'fields':['team_name','leader','members']}),
+        ('Checking Details', {'fields':['quiz_submitted_at','exam_checked','get_marks']}),
+    ]
+    readonly_fields = ('reg_id','payment_id','quiz_submitted_at','get_marks')
+    inlines = [SolutionAdmin]
+
+admin.site.register(Quiz,QuizAdmin)
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Registration,RegisteredAdmin)
--- medquiz/models.py
+++ medquiz/models.py
@@ -1,3 +1,96 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
+from django.utils.html import mark_safe
 # Create your models here.
+
+class Quiz(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4)
+    title = models.CharField("Quiz Title",max_length=50)
+    desc = models.TextField("About Quiz",max_length=500)
+    reg_price = models.IntegerField("Registration Cost(if any)",default=0)
+    team_cnt = models.IntegerField("Max Members In 1 Team",default=1)
+    reg_open = models.BooleanField("Registration Opened?",default=False)
+    quiz_live = models.BooleanField("Quiz Started?",default=False)
+    on_web = models.BooleanField("Conduct on SciSoc Website?",default=True)
+    quiz_or_reg_link = models.URLField("Link for Registration/Quiz",null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Medillectuals'
+
+class Team_Members(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Registration(models.Model):
+    reg_id = models.UUIDField(primary_key = True, default = uuid.uuid4)
+    registered = models.BooleanField("Registration Valid?",default=False)
+    payment_id = models.CharField("Payment Details",max_length=200,null=True,blank=True)
+    quiz_model = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    team_name = models.CharField("Team Name",max_length=25)
+    leader = models.ForeignKey(User,on_delete=models.CASCADE,related_name="Team_Leader")
+    members = models.ManyToManyField(Team_Members)
+    quiz_submitted_at = models.DateTimeField("Time Of Quiz Submission",default=None,null=True,blank=True)
+    exam_checked = models.BooleanField("Exam Checked?",default=False)
+
+    def get_marks(self):
+        solns = Solution.objects.filter(reg=self,quiz_id=self.quiz_model)
+        total = 0
+        for soln in solns:
+            if soln.is_correct:
+                total += soln.question_detail.marks
+        return total
+
+    def __str__(self):
+        return self.quiz_model.title + " : " + self.leader.username
+
+
+class Question(models.Model):
+    qid = models.AutoField(primary_key=True)
+    quiz_model = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    question_detail = models.CharField("Question Detail",max_length=250)
+    image = models.ImageField("Question Image",upload_to='anastomosis/',null=True,blank=True)
+    answer = models.TextField("Solution(hided)",null=True,blank=True)
+    marks = models.IntegerField("Marks for correct answer",default=1)
+    choices = models.BooleanField("Single/Multiple Correct Choice?",default=False)
+    short_answer = models.BooleanField("Short Answer Subjective Question?",default=False)
+    numerical = models.BooleanField("Numerical/Integer?",default=False)
+    true_false = models.BooleanField("True False Question?",default=False)
+    matchup = models.BooleanField("Match The Following Question?",default=False)
+    num_match = models.IntegerField("Number of Match options",default=0)
+
+    def __str__(self):
+        return self.question_detail
+
+    def delete(self):
+        self.image.storage.delete(self.image.name)
+        super().delete()
+    
+    def qn_image(self):
+        return mark_safe('<img src="/media/%s" width="250" max-height="250" />' % (self.image))
+    qn_image.short_description = 'Image'
+
+
+class Choice(models.Model):
+    question_id = models.ForeignKey(Question,on_delete=models.CASCADE)
+    option = models.CharField("Choice Detail",max_length=50)
+    is_correct = models.BooleanField("Correct Choice? (hided)",default=False)
+
+    def __str__(self):
+        return self.option
+
+class Solution(models.Model):
+    reg = models.ForeignKey(Registration,on_delete=models.CASCADE)
+    quiz_id = models.ForeignKey(Quiz,on_delete=models.CASCADE,verbose_name="Quiz Name")
+    question_detail = models.ForeignKey(Question,on_delete=models.CASCADE,verbose_name="Question")
+    sol_by_participant = models.TextField("Solution By Participant",null=True,blank=True)
+    is_correct = models.BooleanField("Mark Correct",default=False)
+
+    def qn_image(self):
+        return mark_safe('<img src="/media/%s" width="250" max-height="250" />' % (self.question_detail.image))
+    qn_image.short_description = 'Image'
