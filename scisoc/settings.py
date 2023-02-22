331930ecf89a25fdfc25789830cd17d0396a7ff0
unknown <goyalaayush93@gmail.com>
2023-02-22 17:10:09 +0530

    Static Updated

--- scisoc/settings.py
+++ scisoc/settings.py
@@ -126,9 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
+if DEBUG:
+    STATICFILES_DIRS=[
+        os.path.join(BASE_DIR,'static')
+    ]
+else:
+    STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
